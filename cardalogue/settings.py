797061e3cd8aae07d39d93fd2a3f35178bac4b17
jmason87 <joewebdev87@gmail.com>
2022-03-24 10:41:56 -0500

    added to whitelist

--- cardalogue/settings.py
+++ cardalogue/settings.py
@@ -58,7 +58,9 @@ REST_FRAMEWORK = {
 # THIS IS NEW
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://cardalogue-server.herokuapp.com'
+    'https://cardalogue.herokuapp.com'
 )
 
 MIDDLEWARE = [
