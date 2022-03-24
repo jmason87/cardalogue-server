6b5cb3e5ab064d9ea3d5fef49e88ae7e820dca46
jmason87 <joewebdev87@gmail.com>
2022-03-24 13:32:51 -0500

    ...forgot a comma

--- cardalogue/settings.py
+++ cardalogue/settings.py
@@ -59,7 +59,7 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://cardalogue-server.herokuapp.com'
+    'https://cardalogue-server.herokuapp.com',
     'https://thecardalogue.herokuapp.com'
 )
 
